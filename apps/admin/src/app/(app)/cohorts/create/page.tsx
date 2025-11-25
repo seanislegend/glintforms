5b0caf7d00ba81c2406d7bc4d65a735047b11d7c
Sean McEmerson <boredofmath+github@gmail.com>
2025-11-25 17:27:33 +0000

    fix: Suspend client component

--- apps/admin/src/app/(app)/cohorts/create/page.tsx
+++ apps/admin/src/app/(app)/cohorts/create/page.tsx
@@ -1,6 +1,7 @@
 import Container from '@glint/ui/container';
 import SectionHeader from '@glint/ui/section-header';
 import Spacer from '@glint/ui/spacer';
+import {Suspense} from 'react';
 import {HydrateClient, prefetch, trpc} from '@/lib/trpc/server';
 import CohortForm from '../form';
 
@@ -12,7 +13,9 @@ const Page: React.FC = async () => {
             <Container>
                 <SectionHeader title="Create new cohort" />
                 <Spacer size="md" />
-                <CohortForm />
+                <Suspense>
+                    <CohortForm />
+                </Suspense>
             </Container>
         </HydrateClient>
     );
